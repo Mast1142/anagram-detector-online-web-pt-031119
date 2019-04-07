ab9f6eee0a0768a57ecdad623772d7fc3a454a84
Samuel T <ssamt1142@gmail.com>
2019-04-07 03:50:21 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+require "pry"
+
+class Anagram
+
+  attr_accessor :data
+
+  def initialize(data)
+    @data = data.split("")
+    #data.chars.sort.join #sort the string alphabetically
+    binding.pry
+  end
+
+
+
+end
